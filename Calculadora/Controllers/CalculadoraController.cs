bb872d868c1f4c6861866fcc1577a95c80084731
Raphael <user@DESKTOP-E5CNFAL>
2021-06-09 17:29:57 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calcular()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RealizarCalculo(string operador1, string operacao, string operador2)
+        {
+
+            double op1 = Convert.ToDouble(operador1);
+            double op2 = Convert.ToDouble(operador2);
+            double resultado;
+
+            if(operacao == "+")
+            {
+             resultado = op1 + op2;
+            }
+            else if (operacao == "-")
+            {
+                resultado = op1 - op2;
+            }
+            else if (operacao == "/")
+            {
+                resultado = op1 / op2;
+            }
+            else if (operacao == "*" || operacao == "x")
+            {
+                resultado = op1 * op2;
+            }
+            else
+            {
+                resultado = 0;
+            }
+
+            ViewData["valor"] = resultado;
+
+            return View();
+        }
+    }
+
+}
